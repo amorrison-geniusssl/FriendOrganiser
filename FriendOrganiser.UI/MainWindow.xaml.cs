0526563f79aa577e361c1c2afab6b798625f766d
Alan Morrison <AMorrison@geniusssl.com>
2022-09-27 16:35:10 +0100

    Fixed bug

--- FriendOrganiser.UI/MainWindow.xaml.cs
+++ FriendOrganiser.UI/MainWindow.xaml.cs
@@ -10,16 +10,17 @@ namespace FriendOrganiser.UI
     public partial class MainWindow : Window
     {
         private MainViewModel _viewModel;
-        public MainWindow(INavigationViewModel navigationViewModel)
+        public MainWindow(MainViewModel viewModel)
         {
-            NavigationViewModel = navigationViewModel;
+            InitializeComponent();
+            _viewModel = viewModel;
+            DataContext = _viewModel;
+            Loaded += MainWindow_Loaded;
         }
 
         private async void MainWindow_Loaded(object sender, RoutedEventArgs e)
         {
-            await NavigationViewModel.LoadAsync();
+            await _viewModel.LoadAsync();
         }
-
-        public INavigationViewModel NavigationViewModel { get;}
     }
 }
