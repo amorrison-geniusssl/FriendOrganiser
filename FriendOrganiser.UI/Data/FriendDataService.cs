dbce329ce44fce7e40d757813f5ead741b5ccc3f
Alan Morrison <AMorrison@geniusssl.com>
2022-09-27 10:58:28 +0100

    Basic UI Implemented

---
+++ FriendOrganiser.UI/Data/FriendDataService.cs
@@ -0,0 +1,18 @@
+﻿using FriendOrganiser.Model;
+using System.Collections.Generic;
+
+namespace FriendOrganiser.UI.Data
+{
+    public class FriendDataService : IFriendDataService
+    {
+        public IEnumerable<Friend> GetAll()
+        {
+            // TODO: Load data from real database
+            yield return new Friend { FirstName = "Thomas", LastName = "Huber" };
+            yield return new Friend { FirstName = "Andreas", LastName = "Boehler" };
+            yield return new Friend { FirstName = "Julia", LastName = "Huber" };
+            yield return new Friend { FirstName = "Chrissi", LastName = "Egin" };
+
+        }
+    }
+}
